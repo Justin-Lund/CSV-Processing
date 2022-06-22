793982d5a9dd179e6af34f0f1281e8365a5398ec
Justin Lund <47057520+Justin-Lund@users.noreply.github.com>
2022-06-21 21:04:02 -0700

    Add files via upload

---
+++ CSV-Count-Occurrences.ps1
@@ -0,0 +1,40 @@
+﻿# Count occurrences of a given field in a CSV
+
+#--------------------------------------#
+#              Variables               #
+#--------------------------------------#
+
+# Input filename
+$CSVInput = "Input.csv"
+
+# Output filename
+$CSVOutput = ".\Count_Output.csv"
+
+# CSV header to count occurrences of
+$CSVHeader = "Recipients"
+
+#################################################################
+
+# Import target CSV file
+Import-CSV $CSVInput |
+
+  # Read each line under the target CSV header
+  ForEach-Object $CSVHeader |
+
+  # If multiple objects are listed on one line (eg. in one cell, when viewing in Excel) & comma delimited, separate them
+  ForEach-Object Split ', ' |
+
+  # Group-Object counts the occurrences of each item
+  Group-Object -NoElement |
+
+  # Select only the count & name attributes
+  Select-Object -Property Count, Name |
+
+  # Eliminate blank lines
+  Select-Object | Where Name -ne '' | 
+
+  # Sort by count, with the highest counts on top
+  Sort-Object Count -Descending |
+
+  # Export CSV
+  Export-CSV $CSVOutput -NoTypeInformation
