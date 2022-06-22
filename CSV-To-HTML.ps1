dde10834bd904cc3e3f050c0fa71fd651baf5efb
Justin Lund <47057520+Justin-Lund@users.noreply.github.com>
2022-06-21 21:14:27 -0700

    Add files via upload

---
+++ CSV-To-HTML.ps1
@@ -0,0 +1,30 @@
+﻿# This script will convert all CSV files in the current folder to HTML
+
+##################################################
+
+# One liner to convert a single file:
+# Import-CSV ".\FileToConvert.csv" | ConvertTo-Html | Out-File ".\HTMLOutput.html"
+
+##################################################
+
+# Input/Output stored as variables so that the script can be re-worked for different use cases
+$InputExtension = "csv"
+$OutputExtension = "html"
+
+##################################################
+
+# Create new directory named after the output filetype (eg. html)
+New-Item -Name "$OutputExtension" -ItemType "Directory"
+
+# Search all files in the folder, and filter by filetype (eg. csv)
+Get-ChildItem .\ -Filter *.$InputExtension |
+
+# For each file of the target filetype found:
+ForEach-Object {
+
+    # Set the new file names by removing the input extension (csv) & replacing it with the output extension (html)
+    $NewName = $_.Name.Remove($_.Name.Length - $_.Extension.Length) + ".$OutputExtension"
+
+    # Import CSV file, convert to HTML, & output to new file
+    Import-CSV $_.FullName | ConvertTo-Html | Out-File ".\$OutputExtension\$NewName"
+}
